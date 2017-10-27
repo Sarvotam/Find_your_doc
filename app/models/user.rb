f21a89adb8c9470e2d28d86c9ffdd363bb383b80
Subash Shahi <subashsly33@gmail.com>
2017-10-27 15:28:00 +0545

    final

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 	has_one :doctor
 	has_one :review
+	validates_presence_of :first_name, :Last_name, :username, :address, :date_of_birth, :contact_number, :address, :gender
+	validates_uniqueness_of :username, :contact_number
+	enum gender: [ :male, :female, :others]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :reviews
   devise_for :users
          root to: "reviews#index"
-
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
