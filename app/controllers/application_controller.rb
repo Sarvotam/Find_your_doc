6aab1b2af6ab770ea74de531da07e9ac14cc98a8
Sarvotam <hengajusarvottam@gmail.com>
2017-10-30 17:04:59 +0545

    hait

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	 # before_filter :check_role, only: [:create]
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
  # protected
@@ -20,5 +21,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+
+
+def check_role
+  # if params[:user][:user_type] == 'doctor'
+  #   redirect_to new_doctor_path
+  # # elsif [:user][:user_type] == 'patient'
+  # #   redirect_to user_path
+  # end
+end
  
 end
